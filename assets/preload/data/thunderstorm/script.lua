723a0e4382b4148bd22cdeacb41692a148becb00
HMFNF <51531073+hoaanhmine@users.noreply.github.com>
2022-10-31 21:45:44 +0700

    Update script.lua

--- assets/preload/data/thunderstorm/script.lua
+++ assets/preload/data/thunderstorm/script.lua
@@ -1,19 +1,13 @@
 
 function onStepHit() ----- Character
-    if curStep == 1 then
-        doTweenAlpha('MANIASHAGGY', 'MANIA', 0, 0.5, 'linear');
-    end
-
+    
     if curStep == 557 then
         triggerEvent('Set Cam Zoom', '0.9', '19')
     end
 
-    if curStep == 832 then
-        doTweenAlpha('MANIASHAGGY', 'MANIA', 0.3, 0.5, 'linear');
-    end
+    
     
     if curStep == 961 then
-        doTweenAlpha('MANIASHAGGY', 'MANIA', 0, 0.5, 'linear');
         triggerEvent('Set Cam Zoom', '0.7', '2')
     end
 
@@ -29,12 +23,8 @@ function onStepHit() ----- Character
         triggerEvent('Set Cam Zoom', '0.9', '0.5')
     end
 
-    if curStep == 1215 then
-        doTweenAlpha('MANIASHAGGY', 'MANIA', 0.3, 0.5, 'linear');
-    end
-
+    
     if curStep == 1346 then
-        doTweenAlpha('MANIASHAGGY', 'MANIA', 0, 0.5, 'linear');
         triggerEvent('Set Cam Zoom', '0.7', '0.5')
     end
 
@@ -42,9 +32,7 @@ function onStepHit() ----- Character
         triggerEvent('Set Cam Zoom', '1', '19')
     end
 
-    if curStep == 2035 then
-        doTweenAlpha('MANIASHAGGY', 'MANIA', 1, 0.5, 'linear');
-    end
+
 
 
 end
