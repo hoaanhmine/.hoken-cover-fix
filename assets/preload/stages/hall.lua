3fe475aa31c5bc19928753a9b89ee0bb05b0b16f
HMFNF <51531073+hoaanhmine@users.noreply.github.com>
2022-11-01 12:41:57 +0700

    Update hall.lua

--- assets/preload/stages/hall.lua
+++ assets/preload/stages/hall.lua
@@ -12,7 +12,5 @@ function onCreate()
 	-- addLuaSprite('stagelight_right', false);
 	-- addLuaSprite('stagecurtains', false);
 	
-	-- close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
+close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
 end
-
--- -350 + Math.sin((Conductor.songPosition / 1000) * (Conductor.bpm / 60) * 1.5) * 12.5;
